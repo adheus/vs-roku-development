d2b5ac56cdc8572381af769f1dd15562ed75e481
João Martins <martins.j.neto@gmail.com>
2018-10-23 17:20:44 -0300

    Create deploy and debug task

--- src/extension.ts
+++ src/extension.ts
@@ -4,6 +4,34 @@
 import * as vscode from 'vscode';
 import * as RokuApi from './utils/rokuApi';
 
+function deployApp() {
+    vscode.window.showInformationMessage(`Deploying app to ${RokuApi.getRokuDevice()}`);
+    RokuApi.deploy();
+}
+
+function launchDebug() {
+    const rokuDebugConsoleName = "Roku debug console";
+
+    let rokuConsole = vscode.window.terminals.find(terminal => terminal.name === rokuDebugConsoleName);
+
+    // Close any instances of Roku debug console before running again
+    if (rokuConsole) {
+        rokuConsole.dispose();
+    }
+
+    rokuConsole = vscode.window.createTerminal(rokuDebugConsoleName);
+    rokuConsole.sendText(RokuApi.getDebugConsoleCommand());
+
+    vscode.window.showInformationMessage(`Debug console started for ${RokuApi.getRokuDevice()}`);
+
+    rokuConsole.show();
+}
+
+function deployAndDebug() {
+    launchDebug();
+    deployApp();
+}
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -15,36 +43,15 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let deployDisposable = vscode.commands.registerCommand('extension.deploy', () => {
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage(`Deploying app to ${RokuApi.getRokuDevice()}`);
-        RokuApi.deploy();
-    });
-
-
+    let deployDisposable = vscode.commands.registerCommand('extension.deploy', deployApp);
     context.subscriptions.push(deployDisposable);
 
-    let debugDisposable = vscode.commands.registerCommand('extension.debug', () => {
-        // The code you place here will be executed every time your command is executed
-        const rokuDebugConsoleName = "Roku debug console";
-
-        let rokuConsole = vscode.window.terminals.find(terminal => terminal.name === rokuDebugConsoleName);
-
-        // Close any instances of Roku debug console before running again
-        if (rokuConsole) {
-            rokuConsole.dispose();
-        }
-
-        rokuConsole = vscode.window.createTerminal(rokuDebugConsoleName);
-        rokuConsole.sendText(RokuApi.getDebugConsoleCommand());
-
-        vscode.window.showInformationMessage(`Debug console started for ${RokuApi.getRokuDevice()}`);
-
-        rokuConsole.show();
-    });
-
+    let debugDisposable = vscode.commands.registerCommand('extension.debug', launchDebug);
     context.subscriptions.push(debugDisposable);
+
+    let deployAndDebugDisposable = vscode.commands.registerCommand('extension.deployAndDebug', deployAndDebug);
+    context.subscriptions.push(deployAndDebugDisposable);
+
 }
 
 // this method is called when your extension is deactivated
